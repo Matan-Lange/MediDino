a4a0122b39e82c6e53deba79970cbcf664f823de
matan lange <matanlange88@gmail.com>
2024-09-27 11:56:42 +0000

    finetune code update

--- finetune/dino_model.py
+++ finetune/dino_model.py
@@ -3,12 +3,16 @@ import torch
 
 
 class DinoVisionTransformerClassifier(nn.Module):
-    def __init__(self, hub_path, num_classes=1):
+    def __init__(self, hub_path=None, ssl_dino_path=None, num_classes=1):
         super(DinoVisionTransformerClassifier, self).__init__()
-        self.model = torch.hub.load('facebookresearch/dinov2',
-                                    hub_path)
+        if not ssl_dino_path:
+            self.model = torch.hub.load('facebookresearch/dinov2',
+                                        hub_path)
+        else:
+            self.model = get_dino_finetuned_downloaded(ssl_dino_path)
+
         embeddings_size = self.model.norm.weight.shape[0]
-        # Todo - check making classifier with more params compare to RadImageNet
+
         self.classifier = nn.Sequential(
             nn.Linear(embeddings_size, 256),
             nn.ReLU(),
@@ -20,3 +24,28 @@ class DinoVisionTransformerClassifier(nn.Module):
         x = self.model.norm(x)
         x = self.classifier(x)
         return x
+
+
+def get_dino_finetuned_downloaded(dino_path_finetuned):
+    model=torch.hub.load('facebookresearch/dinov2', 'dinov2_vits14')
+
+    pretrained = torch.load(dino_path_finetuned, map_location=torch.device('cpu'))
+    # make correct state dict for loading
+    new_state_dict = {}
+    for key, value in pretrained['teacher'].items():
+        if 'dino_head' in key:
+            print('not used')
+        else:
+            new_key = key.replace('backbone.', '')
+            new_state_dict[new_key] = value
+    #change shape of pos_embed, shape depending on vits or vitg
+    pos_embed = nn.Parameter(torch.zeros(1, 257, 384))
+    model.pos_embed = pos_embed
+    # load state dict
+    model.load_state_dict(new_state_dict, strict=True)
+    return model
+
+
+
+
+
--- finetune/train.py
+++ finetune/train.py
@@ -12,13 +12,16 @@ from torchvision import transforms
 def parse_parameters():
     parser = argparse.ArgumentParser(description="Script to train a model using Dino")
     parser.add_argument('--dataset', type=str, required=True, help='Path to the dataset')
-    parser.add_argument('--dino_version', type=str, required=True, help='Version of Dino to use')
     parser.add_argument('--epochs', type=int, default=30, help='Number of epochs to train for (default: 10)')
     parser.add_argument('--lr', type=float, default=1e-6, help='Learning rate for training (default: 0.001)')
     parser.add_argument('--batch_size', type=int, default=32, help='Batch size for training (default: 32)')
     parser.add_argument('--device', type=str, default='cuda', help='Device to use')
     parser.add_argument('--neg_label', type=str, help='Negative label in dataframe')
     parser.add_argument('--pos_label', type=str, help='Positive label in dataframe')
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--dino_version', type=str, help='Version of Dino to use')
+    group.add_argument('--ssl_model_path', type=str, help='Path to the custom_SSL_DINO model')
 
     return parser.parse_args()
 
@@ -46,8 +49,8 @@ def get_transforms():
 
 if __name__ == "__main__":
     params = parse_parameters()
-    base_path = f'/home/MediDino/data{params.dataset}/'
-    csv_path = f'/home/MediDino/data{params.dataset}/dataframe/'
+    base_path = f'/home/MediDino/data/{params.dataset}/'
+    csv_path = f'/home/MediDino/data/{params.dataset}/dataframe/'
 
     train_transforms, val_transforms = get_transforms()
 
@@ -69,7 +72,10 @@ if __name__ == "__main__":
                                      transform=val_transforms,
                                      label_map={params.neg_label: 0, params.pos_label: 1})
 
-        model = DinoVisionTransformerClassifier(hub_path=params.dino_version)
+        if params.ssl_model_path:
+            model = DinoVisionTransformerClassifier(ssl_dino_path=params.ssl_model_path)
+        else:
+            model = DinoVisionTransformerClassifier(hub_path=params.dino_version)
         model.train()
         trainer = Trainer(model=model,
                           device=params.device,
